0389110cf5c39aaa2d305a4d749fcc5becf0c581
José Lucas <zelucasr@gmail.com>
2017-06-01 20:24:16 -0300

    adicionando a classe Grid

---
+++ src/model/Grid.java
@@ -0,0 +1,108 @@
+package model;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import util.MyPopupMenu;
+import util.MyPopupTriggerListener;
+import util.MyActionListener;
+
+public class Grid {
+	static Grid grid = null;
+	
+	static Node[][] matrix;
+	static int columns;
+	static int lines;
+	
+	static JFrame main_frame;
+	static JButton[][] nodes;
+	
+	static Node source;
+	static Node actual;
+	static Node destiny;
+	
+	static Color actualColor = new Color(155, 205, 255);
+	static Color visitedColor = new Color(205, 230, 255);
+	static Color noOptionsColor = new Color(255, 178, 102);
+	
+	static Color sourceColor = new Color(204, 153, 255);
+	static Color destinyColor = new Color(255, 205, 230);
+	
+	static Color validColor = new Color(155, 255, 155);
+	static Color invalidColor = new Color(255, 155,155);
+	
+	static long delay = 5;
+	public static int bombPower = 1;
+	
+	static State state = new State();
+	
+	final JFileChooser fc = new JFileChooser("C:\\");
+	
+	public static Grid getSharedInstance() {
+		if (grid == null) {
+			@SuppressWarnings("resource")
+			Scanner entrada = new Scanner(System.in);
+			System.out.print("Digite a altura do Grid: ");
+			int lines = entrada.nextInt();
+			System.out.print("Digite a larguta do Grid: ");
+			int columns = entrada.nextInt();
+			grid = new Grid(lines, columns);
+			return grid;
+		} else {
+			return grid;
+		}
+	}
+	
+	public Grid(int lines, int columns) {
+		matrix = new Node[lines][columns];
+		Grid.columns = columns;
+		Grid.lines = lines;
+		
+		for(int i = 0; i < lines; i++) {
+			for(int j = 0; j < columns; j++){
+				matrix[i][j] = new Node(i,j, columns);
+			}
+		}
+	}
+	
+	public void happyPath() throws InterruptedException {
+		
+	}
+	
+	public Point getBestDistance() { //Pega o ponto com a melhor distância
+		return new Point();
+	}
+	
+	public Point getLower(ArrayList<Point> points) { 
+		return new Point();
+	}
+	
+	public ArrayList<Point> calcDistances(){
+		return new ArrayList<Point>();
+	}
+	
+	public boolean wasVisited(Point p) {
+		return (matrix[p.i][p.j].visited);
+	}
+	
+	public boolean isAdjacent(int id1, int id2) {	
+		return true;
+	}
+
+	public static void main(String args[]) throws InterruptedException {
+		Grid.getSharedInstance();
+	}
+}
