b9f7a9f64955e6a878b673d77ebb69525493fb07
Fernanda Chacon <fernandachaconfontoura@gmail.com>
2017-06-05 16:57:08 -0300

    adicionando classe MyMouseListener

---
+++ src/util/MyMouseListener.java
@@ -0,0 +1,44 @@
+package util;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MyMouseListener implements MouseListener{
+	public int i, j;
+	
+	public MyMouseListener(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
