6acf4c8b4d343f11e166abd5d2a7b7e5ac9ad3a2
Romildo <jrmildo@gmail.com>
2017-06-20 23:29:23 -0300

    corrigindo problem com MyPopupMenu

--- src/util/MyPopupMenu.java
+++ src/util/MyPopupMenu.java
@@ -11,22 +11,22 @@ import model.Grid;
 
 public class MyPopupMenu extends JPopupMenu{
 	private static final long serialVersionUID = 1L;
-	public int i, j;
+	public final int i, j;
 	public MyPopupMenu(int i, int j) {
 		super();
 		this.i = i;
 		this.j = j;
-		
+
 		JMenuItem source = new JMenuItem("Set as Source");
 		source.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				Grid.setSource(i, j);
 			}
-			
+
 		});
 		this.add(source);
-		
+
 		JMenuItem destiny = new JMenuItem("Set as Destiny");
 		destiny.addActionListener(new ActionListener() {
 			@Override
@@ -35,7 +35,7 @@ public class MyPopupMenu extends JPopupMenu{
 			}
 		});
 		this.add(destiny);
-		
+
 		JMenuItem bomb = new JMenuItem("Drop bomb here");
 		bomb.addActionListener(new ActionListener() {
 			@Override
