cc260492e7824ff7f301a0f530df98aa3405d192
Romildo <jrmildo@gmail.com>
2017-06-20 23:05:03 -0300

    adicionando script para testar entradas e saídas

---
+++ script.sh
@@ -0,0 +1,14 @@
+cd src/
+javac ./*/*.java
+
+for f in ../tests/inputs/*; do
+    entrada=`cat $f`
+    java model.Grid $entrada > "../tests/outputs/$(basename "$f" .txt).output"
+    errors=`diff "../tests/outputs/$(basename "$f" .txt).output" "../tests/expected_outputs/$(basename "$f" .txt).expected"`
+    if [[ $errors ]]; then
+        echo -e "\e[31m$f\e[0m\n$errors"
+        exit 1
+    else
+        echo -e "\e[32m$f\e[0m"
+    fi
+done
